241806f45af875ddac0e00fb1e639a3dc1ec2a14
Elizeu RS <13ders@gmail.com>
2023-02-06 14:33:46 -0300

    first commit

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Elizeu RS on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
