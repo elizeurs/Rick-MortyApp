de35c9b60e7018e4dbd96ee55fee27c866ba307f
Elizeu RS <13ders@gmail.com>
2023-02-08 17:20:50 -0300

    API Call

--- RickAndMorty/RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/RickAndMorty/APIClient/RMRequest.swift
@@ -22,7 +22,7 @@ final class RMRequest {
   private let endpoint: RMEndpoint
   
   /// Path components for API, if any
-  private let pathComponents: Set<String>
+  private let pathComponents: [String]
   
   /// Query arguments for API, if any
   private let queryParameters: [URLQueryItem]
@@ -70,7 +70,7 @@ final class RMRequest {
   ///   - queryParameters: Collection of query parameters
   public init(
     endpoint: RMEndpoint,
-    pathComponents: Set<String> = [],
+    pathComponents: [String] = [],
     queryParameters: [URLQueryItem] = []
   ) {
     self.endpoint = endpoint
@@ -79,4 +79,7 @@ final class RMRequest {
   }
 }
 
+extension RMRequest {
+  static let listCharactersRequests = RMRequest(endpoint: .character)
+}
 
--- RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -9,20 +9,40 @@ import UIKit
 
 /// Controller to show and search for Characters
 final class RMCharacterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      view.backgroundColor = .systemBackground
-      title = "Characters"
-      
-      let request = RMRequest(
-        endpoint: .character,
-        queryParameters: [
-          URLQueryItem(name: "name", value: "rick"),
-          URLQueryItem(name: "status", value: "alive")
-        ]
-      )
-      print(request.url)
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    title = "Characters"
+    
+    RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
+      switch result {
+      case .success(let model):
+        print("Total pages: "+String(model.info.pages))
+        print("Total: "+String(model.info.count))
+        print("Page result count: "+String(model.results.count))
+        print(String(describing: model))
+      case .failure(let error):
+        print(String(describing: error))
+      }
     }
-
+    
+//    let request = RMRequest(
+//      endpoint: .character,
+//      queryParameters: [
+//        URLQueryItem(name: "name", value: "rick"),
+//        URLQueryItem(name: "status", value: "alive")
+//      ]
+//    )
+//    print(request.url)
+//
+//    RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+//      switch result {
+//      case .success:
+//        break
+//      case .failure(let error):
+//        print(String(describing: error))
+//      }
+//    }
+  }
 }
