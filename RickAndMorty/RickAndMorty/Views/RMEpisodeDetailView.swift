b4f31fee125dc255d625e11797e1edbe80474267
Elizeu RS <13ders@gmail.com>
2023-02-19 16:11:02 -0300

    Episode Detail View

--- RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
+++ RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -8,15 +8,44 @@
 import UIKit
 
 class RMEpisodeDetailView: UIView {
+  
+  private var viewModel: RMEpisodeDetailViewViewModel?
+  
+  private var collectionView: UICollectionView?
+  
+  private let spinner: UIActivityIndicatorView = {
+    let spinner = UIActivityIndicatorView()
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    return spinner
+  }()
+  
+  // MARK: - Init
 
   override init(frame: CGRect) {
     super.init(frame: frame)
     translatesAutoresizingMaskIntoConstraints = false
     backgroundColor = .systemRed
+    self.collectionView = createCollectionView()
+    addConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("Unsupported")
   }
   
+  private func addConstraints() {
+    NSLayoutConstraint.activate([
+    
+    ])
+  }
+  
+  private func createCollectionView() -> UICollectionView {
+    return UICollectionView()
+  }
+  
+  // MARK: - Public
+  
+  public func configure(with viewModel: RMEpisodeDetailViewViewModel) {
+    
+  }
 }
