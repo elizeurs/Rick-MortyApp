4264b167c99d1cb9da647de9e8be5164972fc823
Elizeu RS <13ders@gmail.com>
2023-02-22 18:43:46 -0300

    Episode Detail Cells

--- RickAndMorty/RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
@@ -13,8 +13,9 @@ protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
 
 final class RMEpisodeDetailViewViewModel {
   private let endpointUrl: URL?
-  private var dataTuple: (RMEpisode, [RMCharacter])? {
+  private var dataTuple: (episode: RMEpisode, characters: [RMCharacter])? {
     didSet {
+      createCellViewModels()
       delegate?.didFetchEpisodeDetails()
     }
   }
@@ -27,17 +28,38 @@ final class RMEpisodeDetailViewViewModel {
   public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
   
   // public private(set) - public property, but not for writing to it, only for reading to it.
-  public private(set) var sections: [SectionType] = []
+  public private(set) var cellViewModels: [SectionType] = []
   
   // MARK: - Init
   
   init(endpointUrl: URL?) {
     self.endpointUrl = endpointUrl
   }
-  
-  // MARK: - Public
-  
+    
   // MARK: - Private
+  
+  private func createCellViewModels() {
+    guard let dataTuple = dataTuple else {
+      return
+    }
+    
+    let episode = dataTuple.episode
+    let characters = dataTuple.characters
+    cellViewModels = [
+      .information(viewModels: [
+        .init(title: "Episode Name", value: episode.name),
+        .init(title: "Air Date", value: episode.air_date),
+        .init(title: "Episode", value: episode.episode),
+        .init(title: "Created", value: episode.created)
+      ]),
+      .characters(viewModel: characters.compactMap({ character in
+        return RMCharacterCollectionViewCellViewModel(
+          characterName: character.name,
+          characterStatus: character.status,
+          characterImageUrl: URL(string: character.image))
+      }))
+    ]
+  }
   
   /// Fetch backing episode model
   public func fetchEpisodeData() {
@@ -86,7 +108,10 @@ final class RMEpisodeDetailViewViewModel {
     }
     
     group.notify(queue: .main) {
-      
+      self.dataTuple = (
+        episode: episode,
+        characters: characters
+      )
     }
   }
 }
