e4abb5ba2fa66fd0f6ce2b74f67b45b3d19e752c
Elizeu RS <13ders@gmail.com>
2023-04-08 18:11:00 -0300

    Wrap up location pagination + device families

--- RickAndMorty/RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
@@ -40,9 +40,15 @@ final class RMLocationViewViewModel {
   
   public var isLoadingMoreLocations = false
   
+  private var didFinishPagination: (() -> Void)?
+  
   // MARK: - Init
   
   init() {}
+  
+  public func registerDidFinishPaginationBlock(_ block: @escaping () -> Void) {
+    self.didFinishPagination = block
+  }
   
   /// Paginate, if additional episodes are needed
   public func fetchAdditionalLocations() {
@@ -71,13 +77,17 @@ final class RMLocationViewViewModel {
       case .success(let responseModel):
         let moreResults = responseModel.results
         let info = responseModel.info
-        print("More locations: \(moreResults.count)")
+        //        print("More locations: \(moreResults.count)")
         strongSelf.apiInfo = info
+        print(info.next)
         strongSelf.cellViewModels.append(contentsOf: moreResults.compactMap({
           return RMLocationTableViewCellViewModel(location: $0)
         }))
         DispatchQueue.main.async {
-           strongSelf.isLoadingMoreLocations = false
+          strongSelf.isLoadingMoreLocations = false
+          
+          // Notify via callback
+          strongSelf.didFinishPagination?()
         }
       case .failure(let failure):
         print(String(describing: failure))
--- RickAndMorty/RickAndMorty/Views/Location/RMLocationView.swift
+++ RickAndMorty/RickAndMorty/Views/Location/RMLocationView.swift
@@ -23,6 +23,15 @@ final class RMLocationView: UIView {
       UIView.animate(withDuration: 0.3) {
         self.tableView.alpha = 1
       }
+      
+      viewModel?.registerDidFinishPaginationBlock { [weak self] in
+        DispatchQueue.main.async {
+          // Loading indicator go bye bye
+          self?.tableView.tableFooterView = nil
+          // Reload data
+          self?.tableView.reloadData()
+        }
+      }
     }
   }
   
@@ -134,11 +143,6 @@ extension RMLocationView: UIScrollViewDelegate {
           self?.showLoadingIndicator()
         }
         viewModel.fetchAdditionalLocations()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
-          print("Refreshing table rows")
-          self?.tableView.reloadData()
-        })
       }
       t.invalidate()
     }
